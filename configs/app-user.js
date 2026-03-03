fe5a207b85c7caf66a4a2f8033075b698a3f01e1
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2026-03-02 19:44:41 -0600

    Fix: Limpieza de sintaxis, ruta integrada y merge abortado

--- configs/app-user.js
+++ configs/app-user.js
@@ -7,18 +7,15 @@ import morgan from 'morgan';
 import { dbConnection } from './db.js';
 import { corsOption } from './cors-configuration.js';
 
-// rutas de cliente
-import usersRoutes from '../scr/users/users.routes.js';
-import accountsRoutes from '../scr/accounts/accounts.routes.js';
-import transfersRoutes from '../scr/transfers/transfers.routes.js';
-<<<<<<< HEAD
-import productsRoutes from '../scr/products/products.routes.js';
-import shoppingsRoutes from '../scr/shoppings/shoppings.routes.js';
-=======
-import favoriteRoutes from "../scr/favorites/favorites.routes.js";
-import currenciesRoutes from '../scr/currencies/currencies.routes.js';
+// rutas de cliente 
+import usersRoutes from '../src/users/users.routes.js';
+import accountsRoutes from '../src/accounts/accounts.routes.js';
+import transfersRoutes from '../src/transfers/transfers.routes.js';
+import productsRoutes from '../src/products/products.routes.js';
+import shoppingsRoutes from '../src/shoppings/shoppings.routes.js';
+import favoriteRoutes from "../src/favorites/favorites.routes.js";
+import currenciesRoutes from '../src/currencies/currencies.routes.js';
 
->>>>>>> 7881ea33ab8a6d73d7e987554efa686c69a84341
 
 const setupMiddlewares = (app) => {
     app.use(helmet());
@@ -35,13 +32,10 @@ const setupRoutes = (app) => {
     app.use(`${BASE_URL}/users`, usersRoutes);
     app.use(`${BASE_URL}/accounts`, accountsRoutes);
     app.use(`${BASE_URL}/transfers`, transfersRoutes);
-<<<<<<< HEAD
     app.use(`${BASE_URL}/products`, productsRoutes);
     app.use(`${BASE_URL}/shoppings`, shoppingsRoutes);
-=======
     app.use(`${BASE_URL}/currencies`, currenciesRoutes);
     app.use(`${BASE_URL}/favorites`, favoriteRoutes);
->>>>>>> 7881ea33ab8a6d73d7e987554efa686c69a84341
 
     app.get(`${BASE_URL}/check`, (req, res) => {
         res.status(200).json({ message: 'NovaPay User Server is up and running' });
